266e2c4ebb857bc88b0d54f04d8ec0c9e46509b2
Patrick Paysant <patrick@armage.org>
2014-02-09 17:57:53 +0100

    Move code to get lang from utils.php to index.php

--- utils.php
+++ utils.php
@@ -8,23 +8,17 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * Sacaliens is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-
-define('DEBUG', true) ;
-
-$lang = DEFAULT_LANG ;
-// $lang = getLang() ;
-loadLang($lang) ;
 
 function ajax_debug($var, $text="") {
       if (DEBUG) {
